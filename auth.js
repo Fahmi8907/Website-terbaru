212bec42265505d31e88d17f0abb7150b44b9bf4
fahmzy <fahmiakbar097@gmail.com>
2025-02-19 19:12:10 +0700

    update

--- auth.js
+++ auth.js
@@ -45,3 +45,20 @@ socket.on("play-audio", () => {
 socket.on("pause-audio", () => {
     document.getElementById("music").pause();
 });
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch(error => alert(error));
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+});
